f98442602a289f67f55ad9bb3ebc65370b9e09ca
EugenioUrbano <urbanoe348@gmail.com>
2024-08-07 07:22:50 -0300

    agr sim é a base real

--- app/views.py
+++ app/views.py
@@ -4,13 +4,7 @@ def index(request):
     return render(request, "index.html")
 
 def jogadores(request):
-    jogadores = [
-        {"Nome": "Lionel Messi", "idade": "32"}
-    ]
-    context = {
-        "atletas": jogadores,
-    }
-    return render(request, "sobre.html"),
+    return render(request, "sobre.html")
 
 def sobre(request):
     return render(request, "sobre.html")
